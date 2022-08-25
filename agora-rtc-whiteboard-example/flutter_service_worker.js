0dce4bd880a5321696dfb1f60a0b50395cf2f49d
nguyenhoangvannha <nguyenhoangvannha@gmail.com>
2022-08-25 13:56:34 +0700

    agora-rtc-whiteboard-example

--- agora-rtc-whiteboard-example/flutter_service_worker.js
+++ agora-rtc-whiteboard-example/flutter_service_worker.js
@@ -4,8 +4,8 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "version.json": "1ed3a09e10517735246281ad1724e062",
-"index.html": "0178f75a7c6bfd29060d943dfe0b2c46",
-"/": "0178f75a7c6bfd29060d943dfe0b2c46",
+"index.html": "e1f2ab56a0b2cafe1514c710a011ea14",
+"/": "e1f2ab56a0b2cafe1514c710a011ea14",
 "main.dart.js": "78dafcdc5cc538ddf3204759bb8e5e04",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
@@ -17,7 +17,11 @@ const RESOURCES = {
 "assets/NOTICES": "172661f7a237bd44c35101faf594c151",
 "assets/FontManifest.json": "dc3d03800ccca4601324923c0b1d6d57",
 "assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "6d342eb68f170c97609e9da345464e5e",
-"assets/fonts/MaterialIcons-Regular.otf": "4e6447691c9509f7acdbf8a931a85ca1"
+"assets/fonts/MaterialIcons-Regular.otf": "4e6447691c9509f7acdbf8a931a85ca1",
+"canvaskit/canvaskit.js": "43fa9e17039a625450b6aba93baf521e",
+"canvaskit/profiling/canvaskit.js": "f3bfccc993a1e0bfdd3440af60d99df4",
+"canvaskit/profiling/canvaskit.wasm": "a9610cf39260f60fbe7524a785c66101",
+"canvaskit/canvaskit.wasm": "04ed3c745ff1dee16504be01f9623498"
 };
 
 // The application shell files that are downloaded before a service worker can
